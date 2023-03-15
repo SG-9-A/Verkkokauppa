90eff26de728b6302fd38d2c45eaea5b77af1b23
kainukai <117354315+kainukai@users.noreply.github.com>
2023-03-15 23:53:31 +0200

    Add files via upload
    Lisätty teksti tuotesivulle

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import BootstrapCarousel from '../components/Carousel'  
 
-export default function Home() {
-  return (
-    <div>Products</div>
-  )
+class MyHeader extends React.Component {
+  render() {
+    const mystyle = {
+      color: "red",
+      padding: "200px",
+      fontFamily: "Arial",
+    };
+    return (
+      <div>
+      <h1 style={mystyle}>Tuotteet oliki etusivulla tässä vaiheessa:Dlol</h1>
+      </div>
+    );
+  }
 }
+
+export default MyHeader;
