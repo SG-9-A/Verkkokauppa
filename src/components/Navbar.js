fba52ede8282bbba1413eacf7fc0db42688ccbe1
kainukai <117354315+kainukai@users.noreply.github.com>
2023-03-16 12:49:37 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,11 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">KerkkoVauppa</a>
+                <img
+                    className='navbar-brand'
+                    src={require("../img/logo.jpg")}
+                    height="100px" />
+
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -15,12 +19,12 @@ export default function Navbar() {
                             <Link className="nav-link" to="/">Etusivu</Link>
                         </li>
                         <li className="nav-item">
-                        <Link className="nav-link" to="/products">Tuotteet</Link>
+                            <Link className="nav-link" to="/products">Tuotteet</Link>
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                            <button className="btn btn-outline-success" type="submit">Haku</button>
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Haku</button>
                     </form>
                 </div>
             </div>
